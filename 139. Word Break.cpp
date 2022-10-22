e27759e1ef667a8de2b8017c3643e47287642206
Anirban Dutta <anirbandutta428@gmail.com>
2022-10-22 22:16:10 +0530

    Create 139. Word Break.cpp

---
+++ 139. Word Break.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1, false);
+        dp[0] = true;
+        
+        // we mark as true every index that we managed to segment so far
+        for (int i = 1; i <= s.size(); i++)
+            for (int j = 0; j < i; j++)
+                if ((dp[j]) && (find(wordDict.begin(), wordDict.end(), s.substr(j, i-j)) != wordDict.end())) {
+                    cout<<s.substr(j, i-j)<<endl;
+                    dp[i] = true;
+                    
+                    
+                }
+        
+        return dp[s.size()];
+    }
+};
